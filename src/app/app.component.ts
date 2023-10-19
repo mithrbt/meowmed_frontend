801d9d99dc8591f94e7146e87f1581149e051624
KrissRose <altmann.kristin.ka@gmail.com>
2023-10-19 16:08:28 +0200

    Revert "Mitarbeiter-dashboard zum Daten an Datenbank senden (nur Dashboard ohne Verbindungen)"
    This reverts commit 4cc053231be307e53d918f018b27c49bff49afdd.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
-import { MitarbeiterComponent } from './mitarbeiter/mitarbeiter.component';
+
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [MitarbeiterComponent  ],
-  template: `
-  <main>
-    <header class="brand-name">
-      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-    </header>
-    <section class="content">
-      <app-mitarbeiter></app-mitarbeiter>
-    </section>
-  </main>
-`,
-  styleUrls: ['./app.component.css'],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mitarbeiterdashboard';
+  title = 'meowmed-frontend';
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-/*
-*  Protractor support is deprecated in Angular.
-*  Protractor is used in this example for compatibility with Angular documentation tools.
-*/
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
